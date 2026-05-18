ec1603c0b738dfc967c5cf315688c48749f97535
nishisharma2025 <nishi.sharma2@ibm.com>
2026-05-18 09:59:40 +0530

    Add Jython automation script for PM work order validation

---
+++ predictive-maintenance-20260518-000000/scripts/pm-workorder-validation-script.py
@@ -0,0 +1,8 @@
+from psdi.server import MXServer
+if mbo is not None:
+    asset = mbo.getString("ASSETNUM")
+    pmnum = mbo.getString("PMNUM")
+    if not asset:
+        service.log("Missing asset for PM")
+    if mbo.isNew():
+        service.log("PM generated Work Order: " + pmnum)
